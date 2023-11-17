899d0e270e860f16e6087ce4f805bcf1378ba65a
Victor Rocha <victor.rocha2002@gmail.com>
2023-11-17 14:48:47 -0300

    Adicionando cors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const knex = require('./conexao');
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 
 app.use(express.json());
-
 app.get('/', async(req, res)=>{
     
     try{
