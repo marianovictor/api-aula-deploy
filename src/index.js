1969983ebf57964a7e083e131bd3648cba34424f
Victor Rocha <victor.rocha2002@gmail.com>
2023-11-17 14:12:23 -0300

    adicionado array de carros

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-
+const carros = ['S10', 'Hilux', 'Ford Ranger'];
 app.use(express.json());
 
 app.get('/', async(req, res)=>{
-    return res.json('API OK CYCLIC!');
+    return res.json(carros);
 })
 
 
